71441a0347ad57621dc1ab9bd43d69db9d1e8a95
Leandro Correia <leandro.correia@gmail.com>
2024-03-15 14:03:51 -0300

    FIX: corrigido erro de sintaxe no script SQL - auto_increment exige uma KEY - drive-by: padronizacao das aspas

--- Bancos/db/CreateDatabase.sql
+++ Bancos/db/CreateDatabase.sql
@@ -3,10 +3,11 @@ CREATE TABLE user (
     id integer not null auto_increment,
     is_active boolean not null,
     created_date datetime not null,
-    name varchar(200)
+    name varchar(200),
+    PRIMARY KEY (id)
 );
 SET character_set_client = utf8;
 SET character_set_connection = utf8;
 SET character_set_results = utf8;
 SET collation_connection = utf8_general_ci;
-INSERT INTO user (is_active, created_date, name) VALUES (1, "2018-07-21", 'João Fulano');
+INSERT INTO user (is_active, created_date, name) VALUES (1, '2018-07-21', 'João Fulano');
